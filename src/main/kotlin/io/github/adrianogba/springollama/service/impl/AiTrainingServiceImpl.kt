a036f53e4cd5da4711842236afa8f79ba5938920
Adriano Pontes <ampontes93@gmail.com>
2024-03-31 19:03:05 -0300

    moving model to environment variable

--- src/main/kotlin/io/github/adrianogba/springollama/service/impl/AiTrainingServiceImpl.kt
+++ src/main/kotlin/io/github/adrianogba/springollama/service/impl/AiTrainingServiceImpl.kt
@@ -6,18 +6,20 @@ import org.springframework.ai.embedding.EmbeddingRequest
 import org.springframework.ai.embedding.EmbeddingResponse
 import org.springframework.ai.ollama.api.OllamaOptions
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 
 @Service
 class AiTrainingServiceImpl @Autowired constructor(
-    private val embeddingClient: EmbeddingClient
+    private val embeddingClient: EmbeddingClient,
+    @Value("\${OLLAMA_MODEL:mistral}") private val model: String
 ) : AiTrainingService {
 
     override fun getEmbeddingsWithModelOverride(payload: String): EmbeddingResponse {
         return embeddingClient.call(EmbeddingRequest(
                 listOf(payload),
-                OllamaOptions.create().withModel("llama2")))
+                OllamaOptions.create().withModel(model)))
     }
 
     override fun getEmbeddingsWithDefaultModel(payload: String): Map<String, EmbeddingResponse> {
