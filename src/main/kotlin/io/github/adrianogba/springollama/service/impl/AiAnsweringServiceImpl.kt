8e4078070baf17988abdc2cecdab01a242ceaac7
Adriano Pontes <ampontes93@gmail.com>
2024-05-30 06:03:28 -0300

    updating dependencies

--- src/main/kotlin/io/github/adrianogba/springollama/service/impl/AiAnsweringServiceImpl.kt
+++ src/main/kotlin/io/github/adrianogba/springollama/service/impl/AiAnsweringServiceImpl.kt
@@ -9,7 +9,6 @@ import org.springframework.ai.ollama.api.OllamaOptions
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
-import java.util.List
 
 
 @Service
